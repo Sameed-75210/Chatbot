9a8a761dce7849431625434602d6079c22a9e956
Sameed <muhammadsameed2002@gmail.com>
2023-02-20 16:43:40 +0500

    Added comments

---
+++ Q1.asm
@@ -0,0 +1,18 @@
+TITLE My First Program (Test.asm)
+INCLUDE Irvine32.inc
+.code
+main PROC
+mov eax, 47h
+add eax, 39
+add eax, 60
+add eax, 85
+add eax, 64
+add eax, 54o
+sub eax, 0Ah
+call DumpRegs
+exit
+main ENDP
+END main
+
+
+;;ajfljaedlaj
